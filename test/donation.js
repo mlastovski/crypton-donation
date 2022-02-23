13dbd38d73cca6c7ee44484d7dea1023b459ad0f
Mark Lastovskyi <emloft@protonmail.com>
2022-02-23 20:00:24 +0200

    removed useless describe

--- test/donation.js
+++ test/donation.js
@@ -18,50 +18,48 @@ describe("Donation contract", function () {
     await donationContract.deployed();
   });
 
-  describe("Donation contract", function () {
-    it("Checks if the contract is deployed", async function () { 
-      await donationContract;
-      expect((await donationContract.address));
-    });
+  it("Checks if the contract is deployed", async function () { 
+    await donationContract;
+    expect((await donationContract.address));
+  });
 
-    it("Should set the right owner", async function () {
-      expect(await donationContract.creatorAddress()).to.equal(owner.address);
-    });
+  it("Should set the right owner", async function () {
+    expect(await donationContract.creatorAddress()).to.equal(owner.address);
+  });
 
-    it("Checks if the balance of the contract is equal to the total sum transacted", async function () {
-      const contract = require("../artifacts/contracts/Donation.sol/Donate.json");
+  it("Checks if the balance of the contract is equal to the total sum transacted", async function () {
+    const contract = require("../artifacts/contracts/Donation.sol/Donate.json");
 
-      const donationNew = new ethers.Contract(
-        donationContract.address, 
-        contract.abi, 
-        owner
-      );
+    const donationNew = new ethers.Contract(
+      donationContract.address, 
+      contract.abi, 
+      owner
+    );
 
-      await donationNew;
-      
-      const tx = {
-        to: donationContract.address,
-        value: ethers.utils.parseEther('2')
-      }
+    await donationNew;
+    
+    const tx = {
+      to: donationContract.address,
+      value: ethers.utils.parseEther('2')
+    }
 
-      const sendFirst = await owner.sendTransaction(tx);
-      const sendSecond = await addr2.sendTransaction(tx);
-      await sendFirst;
-      await sendSecond;
-      const balance = await ethers.provider.getBalance(donationContract.address);
-      expect(ethers.utils.formatEther(balance)).to.equal('4.0');
-    });
+    const sendFirst = await owner.sendTransaction(tx);
+    const sendSecond = await addr2.sendTransaction(tx);
+    await sendFirst;
+    await sendSecond;
+    const balance = await ethers.provider.getBalance(donationContract.address);
+    expect(ethers.utils.formatEther(balance)).to.equal('4.0');
+  });
 
-    it("Checks if only owner can withdraw funds from the contract", async function () {
-      await donationContract.connect(owner).withdrawToSpecifiedWallet(addr1.address, '0');
+  it("Checks if only owner can withdraw funds from the contract", async function () {
+    await donationContract.connect(owner).withdrawToSpecifiedWallet(addr1.address, '0');
 
-      let withdraw = false;
-      try {
-        await donationContract.connect(addr1).withdrawToSpecifiedWallet(addr1.address, '0');
-        withdraw = true;
-      } catch(err) {}
+    let withdraw = false;
+    try {
+      await donationContract.connect(addr1).withdrawToSpecifiedWallet(addr1.address, '0');
+      withdraw = true;
+    } catch(err) {}
 
-      expect(withdraw).to.equal(false);
-    });
+    expect(withdraw).to.equal(false);
   });
 });
